cb76d7b850c28b020e858c4c5e467f7c0947ff34
gavin-go-dang <gavin.dang.goldenowl@gmail.com>
2023-06-06 09:45:21 +0700

    Fix code style, avoid to use var, try using spread, arrow operator

--- array_exercise.js
+++ array_exercise.js
@@ -1,17 +1,17 @@
-var numberArray = [1, 5, 13, 6, 9, 22, 24, 21, 101, 100, 200, 4, 7, 7, 12];
-const firstArray = document.getElementById('first_array');
+const numberArray = [1, 5, 13, 6, 9, 22, 24, 21, 101, 100, 200, 4, 7, 7, 12];
+const firstArray = document.getElementById('first-array');
 firstArray.innerHTML = numberArray
 
 
 // Lay cac phan tu > 10 Cach 1
 const filteringArrayGreaterThan10 = [];
-for(var i = 0; i < numberArray.length; i++) {
+for(let i = 0; i < numberArray.length; i++) {
     if (numberArray[i] > 10) {
         filteringArrayGreaterThan10.push(numberArray[i]);
     }
 }
 
-const greaterThan10Elelemt_1 = document.getElementById('element_greater_than_10_1');
+const greaterThan10Elelemt_1 = document.getElementById('element-greater-than-10-1');
 greaterThan10Elelemt_1.innerHTML = filteringArrayGreaterThan10;
 
 
@@ -21,18 +21,23 @@ function checkGreateThan10(number){
 }
 
 const filteringArrayGreaterThan_10_2 = numberArray.filter(checkGreateThan10);
-const greaterThan10_2 = document.getElementById('element_greater_than_10_2');
-greaterThan10_2.innerHTML = filteringArrayGreaterThan_10_2;
-greaterThan10_2.innerHTML = numberArray
 
+const greaterThan10_2 = document.getElementById('element-greater-than-10-2');
+greaterThan10_2.innerHTML = filteringArrayGreaterThan_10_2;
+
+
+// Lay cac phan tu > 10 Cach 3: Su dung arrow function
+const filteringArrayGreaterThan10_3 = numberArray.filter(number => number > 10)
+const greaterThan10_3 = document.getElementById('element-greater-than-10-3');
+greaterThan10_3.innerHTML = filteringArrayGreaterThan_10_2;
 
 //Tinh tong Cach 1: dung for
-var sum = 0;
-for (i = 0; i < numberArray.length; i++){
+let sum = 0;
+for (let i = 0; i < numberArray.length; i++){
     sum += numberArray[i];
 }
 
-const sumArray1 = document.getElementById('sum_array_1');
+const sumArray1 = document.getElementById('sum-array-1');
 sumArray1.innerHTML = sum;
 
 
@@ -43,18 +48,18 @@ function sumReduce(sumBefore, nextElement){
 }
 
 sum = numberArray.reduce(sumReduce, 0);
-const sumArray2 = document.getElementById('sum_array_2');
+const sumArray2 = document.getElementById('sum-array-2');
 sumArray2.innerHTML = sum;
 
 
 //Nhan 2 gia tri trong mang
 //Cach 1: dung for
 const doubleValueList = []
-for (i=0; i < numberArray.length; i++){
+for (let i=0; i < numberArray.length; i++){
     doubleValueList.push(numberArray[i]*2);
 }
 
-var doubleValuesElement1 = document.getElementById('double_value_1');
+const doubleValuesElement1 = document.getElementById('double-value-1');
 doubleValuesElement1.innerHTML = doubleValueList
 
 
@@ -64,7 +69,7 @@ function mapDoubleValue(value){
 }
 
 const doubleValueList2 = numberArray.map(mapDoubleValue)
-const doubleValuesElement2 = document.getElementById('double_value_2');
+const doubleValuesElement2 = document.getElementById('double-value-2');
 doubleValuesElement2.innerHTML = doubleValueList2
 
 
@@ -74,11 +79,11 @@ const numberArray2 = [301, 333, 100, 470, 1000, 290, 80, 39];
 function join2Arrays(array1, array2) {
     const joinArrays = [];
   
-    for (i = 0; i < array1.length; i++) {
+    for (let i = 0; i < array1.length; i++) {
       joinArrays.push(array1[i]);
     }
   
-    for (i = 0; i < array2.length; i++) {
+    for (let i = 0; i < array2.length; i++) {
       joinArrays.push(array2[i]);
     }
   
@@ -87,21 +92,25 @@ function join2Arrays(array1, array2) {
 
 
 const arrayAfterJoin = join2Arrays(numberArray, numberArray2);
-const joinArrayElement_1 = document.getElementById('join_array_1');
+const joinArrayElement_1 = document.getElementById('join-array-1');
 joinArrayElement_1.innerHTML = arrayAfterJoin
 
 
 //Join  2 mang. Cach 2: Dung concat()
 
 const arrayAfterJoin2 = numberArray.concat(numberArray2)
-const joinArrayElement_2 = document.getElementById('join_array_2');
+const joinArrayElement_2 = document.getElementById('join-array-2');
 joinArrayElement_2.innerHTML = arrayAfterJoin2
 
+//Join  2 mang. Cach 3: spread operator
 
+const arrayAfterJoin3 = [...numberArray, ...numberArray2]
+const joinArrayElement_3 = document.getElementById('join-array-3');
+joinArrayElement_3.innerHTML = arrayAfterJoin3
 //Them 1 attribute cho 1 object
 //tao class User voi 3 thuoc tinh: email, username, phone
 
-var user = {'fname' : 'Van A', 'lname' : 'Nguyen'}
+const user = {'fname' : 'Van A', 'lname' : 'Nguyen'}
 const objectBefore = document.getElementById('object-before')
 
 objectBefore.innerHTML = JSON.stringify(user);
